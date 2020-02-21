eb3a163039c31245ebcc35a094c10aef82367edb
Liam Ross <liamross.code@gmail.com>
2020-02-21 08:16:12 -0800

    5. Get basic layout of the application set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
+import { Button, ButtonGroup } from '@pdftron/webviewer-react-toolkit';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main className="app__main">
+        <div className="app__placeholder">Click Load PDF to begin organizing pages.</div>
+      </main>
+      <footer className="app__footer">
+        <ButtonGroup>
+          <Button buttonStyle="borderless">Load PDF</Button>
+          <Button disabled>Download PDF</Button>
+        </ButtonGroup>
+      </footer>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// Set the path to your Web Worker. This will be relative to your html file
+// (which is in `public`) so you will need to path to `'./lib/core'`
+window.CoreControls.setWorkerPath('./lib/core');
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
